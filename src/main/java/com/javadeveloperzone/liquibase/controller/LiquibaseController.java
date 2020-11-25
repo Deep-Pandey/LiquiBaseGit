ab3dc2965bb0bda7b4ebc18fd4c6a4d45b07174c
dpandey5 <dpandey5@LIN25002033.corp.capgemini.com>
2020-11-25 18:03:34 +0530

    Code coverage related changes

--- src/main/java/com/javadeveloperzone/liquibase/controller/LiquibaseController.java
+++ src/main/java/com/javadeveloperzone/liquibase/controller/LiquibaseController.java
@@ -2,15 +2,17 @@ package com.javadeveloperzone.liquibase.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class LiquibaseController {
 
 	
-	@GetMapping
-	public int sum(int a, int b)
+	@GetMapping("/sum/{a}/{b}")
+	public int sum(@PathVariable int a,@PathVariable int b)
 	{
-		return a+b;
+		int o = a+b;
+		return o;
 	}
 	
 	
