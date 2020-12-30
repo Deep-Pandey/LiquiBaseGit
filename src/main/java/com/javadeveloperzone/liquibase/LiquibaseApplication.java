2182a064da46c24b1bcfdc8d1953f53adfce8c09
dpandey5 <dpandey5@LIN25002033.corp.capgemini.com>
2020-12-30 15:23:55 +0530

    externalized war

--- src/main/java/com/javadeveloperzone/liquibase/LiquibaseApplication.java
+++ src/main/java/com/javadeveloperzone/liquibase/LiquibaseApplication.java
@@ -2,9 +2,16 @@ package com.javadeveloperzone.liquibase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LiquibaseApplication {
+public class LiquibaseApplication extends SpringBootServletInitializer {
+
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(LiquibaseApplication.class);
+	 }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiquibaseApplication.class, args);
